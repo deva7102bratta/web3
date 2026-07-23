6581dd39840fb286f1d3115f9db39d0cda5e47cd
deva7102bratta <devabrattayumnam@gmail.com>
2026-07-23 20:29:06 +0530

    seplolia

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,6 +1,16 @@
+import "dotenv/config";
 import hardhatEthers from "@nomicfoundation/hardhat-ethers";
 
 export default {
   solidity: "0.8.28",
   plugins: [hardhatEthers],
+
+  networks: {
+    sepolia: {
+      type: "http",
+      chainType: "l1",
+      url: process.env.SEPOLIA_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
 };
